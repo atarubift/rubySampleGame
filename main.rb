d3465c797ac8002a37246ae872d5065fd5cd58d7
ataruby <atarubift@gmail.com>
2020-12-26 11:38:20 +0900

    Add sprite class

--- main.rb
+++ main.rb
@@ -6,25 +6,42 @@ GROUND_Y = 400
 # 使いたい画像を宣言する
 Image.register(:player, 'images/player.png')
 
+# プレイヤーを表すクラス
+class Player < Sprite
+
+  def initialize
+    x = Window.width / 2
+    y = GROUND_Y - Image[:player].height
+    image = Image[:player]
+    super(x, y, image)
+  end
+
+  # 移動処理
+  def update
+    if Input.key_down?(K_LEFT) && self.x > 0
+      self.x -= 8
+    elsif Input.key_down?(K_RIGHT) && self.x < (Window.width - Image[:player].width)
+      self.x += 8
+    end
+  end
+end
+
+
 Window.load_resources do
 
-  # 変数の初期化
-  x = Window.width / 2 # 初期値を中心にする 
+  # オブジェクト作成
+  player = Player.new
 
   Window.loop do
 
-    # キー入力をチェック
-    if Input.key_down?(K_LEFT) && x > 0
-      x -= 8
-    elsif Input.key_down?(K_RIGHT) && x < (Window.width - Image[:player].width)
-      x += 8
-    end
+    # 移動
+    player.update
 
     # 背景を描画
     Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
     Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
 
     # プレイヤーキャラを描画
-    Window.draw(x, GROUND_Y - Image[:player].height, Image[:player])
+    player.draw
   end
 end
