c4188a123d57e7a7c11d7e907c8ad214f2352409
ataruby <atarubift@gmail.com>
2020-12-26 11:21:33 +0900

    Show Main Char

--- main.rb
+++ main.rb
@@ -3,10 +3,16 @@ include DXOpal
 
 GROUND_Y = 400
 
+# 使いたい画像を宣言する
+Image.register(:player, 'images/player.png')
+
 Window.load_resources do
   Window.loop do
     # 背景を描画
     Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
     Window.draw_box_fill(0, GROUND_Y, Window.width, Window.height, [0, 128, 0])
+
+    # プレイヤーキャラを描画
+    Window.draw(240, GROUND_Y - Image[:player].height, Image[:player])
   end
 end
