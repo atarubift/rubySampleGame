cf837776d73c82321268ae742fa7c7d5e637917d
ataruby <atarubift@gmail.com>
2020-12-26 11:30:31 +0900

    Operate key boad

--- main.rb
+++ main.rb
@@ -9,12 +9,16 @@ Image.register(:player, 'images/player.png')
 Window.load_resources do
 
   # 変数の初期化
-  x = 0
+  x = Window.width / 2 # 初期値を中心にする 
 
   Window.loop do
 
-    # 毎フレーム8ピクセルずつ進む
-    x += 8
+    # キー入力をチェック
+    if Input.key_down?(K_LEFT) && x > 0
+      x -= 8
+    elsif Input.key_down?(K_RIGHT) && x < (Window.width - Image[:player].width)
+      x += 8
+    end
 
     # 背景を描画
     Window.draw_box_fill(0, 0, Window.width, GROUND_Y, [128, 255, 255])
